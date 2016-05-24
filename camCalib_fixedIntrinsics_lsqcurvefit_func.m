91fa4914ea3a68be4b175cb7c64a2dee701e59a3
Catarina Pinto <catarinapp24@gmail.com>
2016-05-25 00:56:27 +0100

    24052016 - 3rd probably

--- camCalib_fixedIntrinsics_lsqcurvefit_func.m
+++ camCalib_fixedIntrinsics_lsqcurvefit_func.m
@@ -26,14 +26,25 @@ ty=t0(2);
 tz=t0(3);
 
 % convert the 3x3 rotation matrix into 3-vector w=[wx wy wz] of the Rodigrues representation
-R=R0;
-theta=acos((trace(R)-1)/2);
-w=(theta/(2*sin(theta)))*[R(3,2)-R(2,3); R(1,3)-R(3,1); R(2,1)-R(1,2)];
+% R=R0;
+% theta=acos((trace(R)-1)/2);
+% if(real(theta) == pi)
+%     theta = 0.00001;
+% end
+% w=(theta/(2*sin(theta)))*[R(3,2)-R(2,3); R(1,3)-R(3,1); R(2,1)-R(1,2)];
+
 wx=w(1);
 wy=w(2);
 wz=w(3);
 
 x0 =[wx, wy, wz, tx, ty, tz];
+
+display('xdata')
+xdata
+display('ydata') 
+ydata
+display('x0')
+x0
 
 options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt');
 lb = [];
