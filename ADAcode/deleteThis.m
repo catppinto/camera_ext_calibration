2452f6cee72bb89b107197f02db6ef500e199193
Catarina Pinto <catarinapp24@gmail.com>
2016-06-27 17:05:28 -0400

    ada new

--- ADAcode/deleteThis.m
+++ ADAcode/deleteThis.m
@@ -42,7 +42,6 @@ end
 
 T_rb2tb = inv(T_tb2rb);
 
-tag_rotation_offset = [rotationAroundY(pi) [0;0;0]; 0 0 0 1];
 
 A = [567.7969970703125, 0.0, 319.5, 0.0; ...
      0.0, 567.7969970703125, 239.5, 0.0;...
@@ -50,7 +49,7 @@ A = [567.7969970703125, 0.0, 319.5, 0.0; ...
      0.0, 0.0, 0.0, 1.0];
 
 transforms = struct('T_w2rb', T_w2rb, 'T_rb2e', T_rb2e , 'T_tb2rb', T_tb2rb, ...
-                    'tag_rotation_offset', tag_rotation_offset, 'A', A, ...
+                    'A', A, ...
                     'tag_height', tag_height);
 
 %% Start procedure
@@ -58,9 +57,8 @@ corners = [];
 
 disp('Perform Physics Based Refinement')
 
-% recover x
 init_K = T_e2c;
-         
+        
 w=rodrigues(init_K(1:3, 1:3));
 wx=w(1);
 wy=w(2);
@@ -73,10 +71,11 @@ options = optimoptions('fmincon', 'Display','iter', 'Algorithm', 'interior-point
 
 [x, fval, exittag] = fmincon(@minimize_tableTagErrorADA,x0,[],[],[],[],[],[],[], options, tags_train.pose, x0, transforms);
 
+%% 
 % get refined extrinsics
 R_est = rodrigues(x(1:3));
-% R_est(3,2) = - R_est(3,2);
-% R_est(2,3) = - R_est(2,3);
+R_est(3,2) = - R_est(3,2);
+R_est(2,3) = - R_est(2,3);
 
 t_est =[x(4);x(5);x(6)];
 
@@ -84,13 +83,9 @@ PB_K = [R_est, t_est ; 0 0 0 1]
 
 init_K
 
-rodrigues(PB_K(1:3, 1:3))
-
-rodrigues(init_K(1:3, 1:3))
-
 %% Parameter Refinement
 
-zhat_table = [0 0 1]';
+zhat_table = [0 0 -1]';
 z_table = tag_height; 
  
 % FROM REAL data
@@ -108,12 +103,6 @@ else
     s = size(tags_train.pose,1);
 end
 
-if(fullcampose)
-    tag_rotation_offset = eye(4);
-else
-    tag_rotation_offset = [rotationAroundY(pi) [0;0;0]; 0 0 0 1];
-end
-
 for i=1:s
              
     if(fullcampose)
@@ -128,11 +117,9 @@ for i=1:s
     end
     
     t_wr = T_w2rb * T_rb2e * init_K * t_c;
-    t_wr = [tag_rotation_offset(1:3,1:3) * t_wr(1:3,1:3) t_wr(1:3,4); 0 0 0 1];
     t_w_estimatesr(:, :, i) =t_wr;
     
     t_wo = T_w2rb * T_rb2e * PB_K * t_c;
-    t_wo = [tag_rotation_offset(1:3,1:3) * t_wo(1:3,1:3) t_wo(1:3,4); 0 0 0 1];
     t_w_estimateso(:, :, i) =t_wo;
     
     zhat_tagr = t_wr(1:3, 3);
@@ -167,10 +154,6 @@ end
 
 %%  find x
 
-PB2_K = [init_K(1:3, 1:3) PB_K(1:3, 4); 0 0 0 1] 
-
-x0 =[PB_K(1,4), PB_K(2,4), PB_K(3,4)];
-
 wld_pose = [tags.worldpose(:, :) ones(size(tags.worldpose,1), 1)]';
 
 if(fullcampose)
@@ -182,22 +165,9 @@ else
     cam_pose = [tags.pose(:, 1:3) ones(size(tags.worldpose,1), 1)]';
 end
 
-wld_pose(1, :) = wld_pose(1, :)+0.04; 
-wld_pose(2, :) = wld_pose(2, :)+0.04; 
-wld_pose(3, :) = wld_pose(3, :)-0.001; 
 
 tag_in_world_gt = T_w2rb * T_rb2tb * wld_pose ;
 
-%% optimization using fmincon
-
-options = optimoptions('fmincon', 'Display','iter', 'Algorithm', 'interior-point');
-
-[x, fval, exittag] = fmincon(@wldpose_minimization_ADA,x0,[],[],[],[],[],[],[], options, tag_in_world_gt, cam_pose, PB2_K, transforms);
-
-% get refined extrinsics
-t_est = x(1:3)';
-
-WK_K = [PB_K(1:3, 1:3), t_est ; 0 0 0 1];
 
 %% show results
 
@@ -205,8 +175,6 @@ WK_K = [PB_K(1:3, 1:3), t_est ; 0 0 0 1];
 init_K
 
 PB_K
-
-WK_K
 
 
 % world poses
@@ -217,8 +185,6 @@ t_w_init =  T_w2rb * T_rb2e * init_K * cam_pose
 
 t_w_PB =  T_w2rb * T_rb2e * PB_K * cam_pose
 
-t_w_WK =  T_w2rb * T_rb2e * WK_K * cam_pose  
-
 
 %% errors 
 
@@ -228,67 +194,9 @@ t_w_WK =  T_w2rb * T_rb2e * WK_K * cam_pose
 [mean_abs_error_PB, std_deviation_PB,  mean_error_3D_PB, std_deviation_3D_PB] = ...
     translationErrorBetweenPointsInWorld(tag_in_world_gt, t_w_PB);
 
-[mean_abs_error_WK, std_deviation_WK,  mean_error_3D_WK, std_deviation_3D_WK] = ...
-    translationErrorBetweenPointsInWorld(tag_in_world_gt, t_w_WK);
+rodrigues(PB_K(1:3 , 1:3))
 
-%% printing
-
-mean_abs_error_i = 1000 * mean_abs_error_i;
-fprintf('Init:\n mean : %3.4f , %3.4f , %3.4f \n ', mean_abs_error_i(1), mean_abs_error_i(2), mean_abs_error_i(3));
-
-std_deviation_i = 1000 * std_deviation_i;
-fprintf('std  : %3.4f , %3.4f , %3.4f \n ', std_deviation_i(1), std_deviation_i(2), std_deviation_i(3));
-
-mean_error_3D_i = 1000 * mean_error_3D_i;
-fprintf('mean : %3.4f \n ', mean_error_3D_i);
-
-std_deviation_3D_i = 1000 * std_deviation_3D_i;
-fprintf('std  : %3.4f \n ', std_deviation_3D_i);
-
-
-mean_abs_error_PB = 1000 * mean_abs_error_PB;
-fprintf('\n\n PB  :\n mean : %3.4f , %3.4f , %3.4f \n ', mean_abs_error_PB(1), mean_abs_error_PB(2), mean_abs_error_PB(3));
-
-std_deviation_PB = 1000 * std_deviation_PB;
-fprintf('std  : %3.4f , %3.4f , %3.4f \n ', std_deviation_PB(1), std_deviation_PB(2), std_deviation_PB(3));
-
-mean_error_3D_PB = 1000 * mean_error_3D_PB;
-fprintf('mean : %3.4f \n ', mean_error_3D_PB);
-
-std_deviation_3D_PB = 1000 * std_deviation_3D_PB;
-fprintf('std  : %3.4f \n ', std_deviation_3D_PB);
-
-mean_abs_error_WK = 1000 * mean_abs_error_WK;
-fprintf('\n\n WK  :\n mean : %3.4f , %3.4f , %3.4f \n ', mean_abs_error_WK(1), mean_abs_error_WK(2), mean_abs_error_WK(3));
-
-std_deviation_WK = 1000 * std_deviation_WK;
-fprintf('std  : %3.4f , %3.4f , %3.4f \n ', std_deviation_WK(1), std_deviation_WK(2), std_deviation_WK(3));
-
-mean_error_3D_WK = 1000 * mean_error_3D_WK;
-fprintf('mean : %3.4f \n ', mean_error_3D_WK);
-
-std_deviation_3D_WK = 1000 * std_deviation_3D_WK;
-fprintf('std  : %3.4f \n ', std_deviation_3D_WK);
-
-%% extra 
-
-PB2_K = [init_K(1:3, 1:3) PB_K(1:3, 4); 0 0 0 1] 
-t_w_PB2 =  T_rb2e * [init_K(1:3, 1:3) PB_K(1:3, 4); 0 0 0 1] * cam_pose;
-[mean_abs_error_PB2, std_deviation_PB2,  mean_error_3D_PB2, std_deviation_3D_PB2] = ...
-    translationErrorBetweenPointsInWorld(tag_in_world_gt, t_w_PB2);
-
-
-mean_abs_error_PB2 = 1000 * mean_abs_error_PB2;
-fprintf('\n\n PB2  :\n mean : %3.4f , %3.4f , %3.4f \n ', mean_abs_error_PB2(1), mean_abs_error_PB2(2), mean_abs_error_PB2(3));
-
-std_deviation_PB2 = 1000 * std_deviation_PB2;
-fprintf('std  : %3.4f , %3.4f , %3.4f \n ', std_deviation_PB2(1), std_deviation_PB2(2), std_deviation_PB2(3));
-
-mean_error_3D_PB2 = 1000 * mean_error_3D_PB2;
-fprintf('mean : %3.4f \n ', mean_error_3D_PB2);
-
-std_deviation_3D_PB2 = 1000 * std_deviation_3D_PB2;
-fprintf('std  : %3.4f \n ', std_deviation_3D_PB2);
+rodrigues(init_K(1:3 , 1:3))
 
 
 
--- ADAcode/minimize_tableTagErrorADA.m
+++ ADAcode/minimize_tableTagErrorADA.m
@@ -14,12 +14,12 @@ end
 zhat_table = [0 0 -1]';
 z_table = tag_height;   
 
-alpha = 1;
-gamma = [0.01 0.01 0.01 0.1 0.1 0.01]';
+alpha = 0.01;
+gamma = [1 1 0.01 0.1 0.1 0.001]';
 psi = 10;
  
 if(fullcampose)
-    tag_rotation_offset = [rotationAroundY(pi) [0;0;0]; 0 0 0 1];
+    tag_rotation_offset = eye(4);
 else
     tag_rotation_offset = [rotationAroundY(pi) [0;0;0]; 0 0 0 1];
 end
@@ -55,29 +55,23 @@ for i=1:s
         
     % for ADA
     t_w = T_w2rb * T_rb2e * K_wc_est * t_c;
-%     t_w = [tag_rotation_offset(1:3,1:3) * t_w(1:3,1:3) t_w(1:3,4); 0 0 0 1];
-       
+    
     zhat_tag = t_w(1:3, 3);
     
     z_tag =  t_w(3, 4);
     counter = counter + 1;
     norm_z(counter) = norm(z_tag - z_table);
-    
-    if(z_tag < 0)
-        negativeZ(counter) = abs(z_tag);
-    else
-        negativeZ(counter) = 0; 
-    end;
-    
+       
     CosTheta = dot(zhat_tag,zhat_table)/(norm(zhat_tag)*norm(zhat_table));
     theta_error(counter) = abs(acos(CosTheta));
        
 end
-    
-r_ext0 = K_ext0(1:3); 
-norm_rExt = [abs( (r_ext0(1) - x(1))) ; 
-             abs( (r_ext0(2) - x(2))) ;
-             abs( (r_ext0(3) - x(3)))];
+
+x(1)
+K_ext0(1)  
+norm_rExt = [ ( x(1) - K_ext0(1)).^2 ; 
+              ( x(2) - K_ext0(2)).^2 ;
+             abs( (K_ext0(3) - x(3)))];
 t_ext0 = K_ext0(4:6); 
 norm_tExt = [abs( (t_ext0(1) - x(4))) ; 
              abs( (t_ext0(2) - x(5))) ;
@@ -85,6 +79,10 @@ norm_tExt = [abs( (t_ext0(1) - x(4))) ;
 norm_ext = [norm_rExt ; norm_tExt];
 
 %% cost function 
+
+norm_z
+theta_error
+norm_ext
  
 F = sum(psi*norm_z) + sum(alpha*theta_error) + sum(gamma.*norm_ext) ;
 
